f1b6d5766365eff444c89026991f4b01e807f75d
afarukcali <afarukcali@gmail.com>
2023-11-17 15:25:35 +0300

    merge nft methods added.

--- timeable_mergeable_nft/src/timeable_mergable_nft.rs
+++ timeable_mergeable_nft/src/timeable_mergable_nft.rs
@@ -1,6 +1,6 @@
 use core::ops::Add;
 
-use alloc::{ string::{ String, ToString }, vec };
+use alloc::{ string::{ String, ToString }, vec::Vec, vec, boxed::Box, format };
 
 use crate::{ error::Error, utils::{ get_current_address, get_key, self }, enums::Address };
 
@@ -25,15 +25,63 @@ use casper_types::{
 use casper_contract::contract_api::{ runtime, storage, system };
 
 // variables
-const OWNER: &str = "owner";
 const COLLECTION: &str = "collection";
 const METADATA: &str = "metadata";
 const TOKEN_ID: &str = "token_id";
+const TOKEN_IDS: &str = "token_ids";
 const NAME: &str = "name";
 
 //entry points
 const ENTRY_POINT_MINT: &str = "mint";
 const ENTRY_POINT_BURN: &str = "burn";
+const ENTRY_POINT_MERGE: &str = "merge";
+
+struct Metadata {
+    name: String,
+    description: String,
+    asset: String,
+}
+
+impl ToString for Metadata {
+    fn to_string(&self) -> String {
+        format!(
+            "Metadata {{ name: {}, description: {}, asset: {} }}",
+            self.name,
+            self.description,
+            self.asset
+        )
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn merge() {
+    let collection: Key = runtime::get_named_arg(COLLECTION);
+    let token_ids: Vec<u64> = runtime::get_named_arg(TOKEN_IDS);
+    let collection_hash: ContractHash = collection.into_hash().map(ContractHash::new).unwrap();
+    let caller: AccountHash = runtime::get_caller();
+
+    for &token_id in &token_ids {
+        burn_nft(collection_hash, token_id);
+    }
+
+    let merged_token_ids: String = token_ids
+        .iter()
+        .map(|&token_id| token_id.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
+
+    let message = format!("We merged {} token ids", merged_token_ids);
+
+    let merged_nft = Metadata {
+        name: String::from("Dappend Merged Nft"),
+        description: message,
+        asset: String::from(
+            "https://ipfs.io/ipfs/bafkreieo73godjrxffwufu36rks7ro3uno4zrkm5vne4bl6jhojyxvhjci"
+        ),
+    };
+
+    mint_nft(collection_hash, Key::Account(caller), merged_nft.to_string());
+}
 
 #[no_mangle]
 pub extern "C" fn burn() {
@@ -48,10 +96,10 @@ pub extern "C" fn burn() {
 pub extern "C" fn mint() {
     let metadata: String = runtime::get_named_arg(METADATA);
     let collection: Key = runtime::get_named_arg(COLLECTION);
-    let token_owner: Key = runtime::get_named_arg(OWNER);
+    let caller: AccountHash = runtime::get_caller();
 
     let collection_hash: ContractHash = collection.into_hash().map(ContractHash::new).unwrap();
-    mint_nft(collection_hash, token_owner, metadata);
+    mint_nft(collection_hash, Key::Account(caller), metadata);
 }
 
 #[no_mangle]
@@ -66,11 +114,7 @@ pub extern "C" fn call() {
 
     let mint_entry_point = EntryPoint::new(
         ENTRY_POINT_MINT,
-        vec![
-            Parameter::new(COLLECTION, CLType::Key),
-            Parameter::new(METADATA, CLType::String),
-            Parameter::new(OWNER, CLType::Key)
-        ],
+        vec![Parameter::new(COLLECTION, CLType::Key), Parameter::new(METADATA, CLType::String)],
         CLType::URef,
         EntryPointAccess::Public,
         EntryPointType::Contract
@@ -84,8 +128,20 @@ pub extern "C" fn call() {
         EntryPointType::Contract
     );
 
+    let merge_entry_point = EntryPoint::new(
+        ENTRY_POINT_MERGE,
+        vec![
+            Parameter::new(COLLECTION, CLType::Key),
+            Parameter::new(TOKEN_IDS, CLType::List(Box::new(CLType::U64)))
+        ],
+        CLType::URef,
+        EntryPointAccess::Public,
+        EntryPointType::Contract
+    );
+
     entry_points.add_entry_point(mint_entry_point);
     entry_points.add_entry_point(burn_entry_point);
+    entry_points.add_entry_point(merge_entry_point);
 
     let hash_name = String::from("dappend_nft_package_hash");
     let uref_name = String::from("dappend_nft_access_uref");
